74b0c8b2955b81d04eb9df8cc48196df7b0de9c7
Tom Rooman <tomrooman@hotmail.com>
2019-08-22 23:13:30 +0200

    redirect all request to /

--- src/index.js
+++ src/index.js
@@ -4,16 +4,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RouterComponent from './components/RouterComponent/RouterComponent';
-import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 
-
-ReactDOM.render(
-  <BrowserRouter>
-    <RouterComponent />
-  </BrowserRouter>,
-  document.querySelector('#rearing-container')
-)
+ReactDOM.render(<RouterComponent />, document.getElementById('rearing-container'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
