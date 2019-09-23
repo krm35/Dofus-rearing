18e5d0d72645094157c4d7d3de049a91e8dfcbba
Tom Rooman <tom.rooman@epitech.eu>
2019-09-23 18:40:36 +0200

    https server

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     // Send API requests on localhost to API server get around CORS.
     proxy: {
       '/api': {
-        target: 'https://localhost:443/',
+        target: 'https://localhost:3001/',
         secure: false,
         changeOrigin: true,
         pathRewrite: { '^/api': '' }
