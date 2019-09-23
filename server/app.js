dc277dbc29d453ee8a01ba393bf0187a389561fe
Tom Rooman <tom.rooman@epitech.eu>
2019-09-23 19:28:56 +0200

    modify proxy url

--- server/app.js
+++ server/app.js
@@ -9,14 +9,14 @@ var app = express();
 
 mongoose.connect('mongodb://localhost/dofus-rearing', { useNewUrlParser: true })
 
-router.all('*', function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header("Access-Control-Allow-Methods", "PUT, GET,POST");
+app.use(logger('dev'));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "dofus-rearing.com"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
